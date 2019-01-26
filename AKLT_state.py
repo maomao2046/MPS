d75d5fcbbeeb3fefd82128f1d19a59cfdb7f17a4
Roy <elkabetzroy@gmail.com>
2019-01-26 20:51:49 +0200

    mps class

--- AKLT_state.py
+++ AKLT_state.py
@@ -1,27 +1,51 @@
 import numpy as np
-import MPS as MPS
+import matrix_product_state as mps
+import matplotlib.pyplot as plt
 
-aklt_1D_mps = {}
+# 1D AKLT
+n = 100
 spin1_tensor = np.array([[[1, 0], [0, 0]], [[0, 1/np.sqrt(2)], [1/np.sqrt(2), 0]], [[0, 0], [0, 1]]])
 singlet = np.array([[0, 1/np.sqrt(2)], [-1/np.sqrt(2), 0]])
-n = 10
-aklt_1D_mps[0] = spin1_tensor
-aklt_1D_mps[1] = singlet
+aklt = mps.MPS('PBC')
+for i in range(n):
+    aklt.add_physical_tensor(spin1_tensor, 0)
+    aklt.add_virtual_tensor(singlet)
+z = np.array([[1, 0, 0], [0, 0, 0], [0, 0, -1]])
+x = 1/np.sqrt(2) * np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
 
-for i in range(2, 2 * n):
-    if np.mod(i - 1, 2):
-        aklt_1D_mps[i] = spin1_tensor
-    if np.mod(i, 2):
-        aklt_1D_mps[i] = singlet
+correlx = []
+correlz = []
+mag_z = []
+mag_x = []
+mag_z.append(aklt.OneSpinMeasurement(z, 0))
+mag_x.append(aklt.OneSpinMeasurement(x, 0))
+for i in range(1, n):
+    mag_z.append(aklt.OneSpinMeasurement(z, i))
+    mag_x.append(aklt.OneSpinMeasurement(x, i))
+    correlx.append(aklt.TwoSpinsMeasurement(0, i, x, x))
+    correlz.append(aklt.TwoSpinsMeasurement(0, i, z, z))
 
-psi_t = np.array(aklt_1D_mps[0])
-for i in range(1, 2 * n):
-    if len(np.shape(aklt_1D_mps[i])) == 3:
-        psi_t = np.tensordot(psi_t, aklt_1D_mps[i], (len(np.shape(psi_t)) - 1, 1))
-    else:
-        psi_t = np.tensordot(psi_t, aklt_1D_mps[i], (len(np.shape(psi_t)) - 1, 0))
+plt.figure()
+plt.title('MAGNETIZATION')
+plt.plot(range(n), mag_z, 'o')
+plt.plot(range(n), mag_x, 'o')
+plt.legend(['mag_z', 'mag_x'])
+plt.show()
 
-psi_t = np.trace(psi_t, 1, len(psi_t.shape) - 1)
+plt.figure()
+plt.title('CORRELATIONS')
+plt.plot(range(1, n), correlx)
+plt.plot(range(1, n), correlz)
+plt.legend(['XX', 'ZZ'])
+plt.show()
+
+
+
+
+
+
+
+
 
 
 
---
+++ matrix_product_state.py
@@ -0,0 +1,101 @@
+import numpy as np
+import copy as cp
+
+
+class MPS:
+
+    def __init__(self, boundary_condition, model_name=None):
+        self.model_name = model_name
+        self.mps = {}
+        self.tensor_count = 0
+        self.bc = boundary_condition
+
+    def add_physical_tensor(self, tensor, physical_leg):
+        if self.bc == 'PBC':
+            if physical_leg == 0:
+                new_shape = (1, 0, 2)
+            if physical_leg == 1:
+                new_shape = (0, 1, 2)
+            if physical_leg == 2:
+                new_shape = (0, 2, 1)
+            new_tensor = np.transpose(tensor, new_shape)
+        if self.bc == 'OPEN':
+            if physical_leg == 0:
+                new_shape == (0, 1)
+            if physical_leg == 1:
+                new_shape == (1, 0)
+            new_tensor == np.transpose(tensor, new_shape)
+        if self.tensor_count > 0:
+            if new_tensor.shape[0] != self.mps[self.tensor_count - 1].shape[1]:
+                raise ('tensors shape do not match')
+        self.mps[self.tensor_count] = new_tensor
+        self.tensor_count += 1
+
+    def add_virtual_tensor(self, tensor):
+        if self.tensor_count > 0 and self.bc == 'PBC':
+            if tensor.shape[0] != self.mps[self.tensor_count - 1].shape[2]:
+                raise ('tensors shape do not match')
+        if self.tensor_count > 0 and self.bc == 'OPEN':
+            if tensor.shape[0] != self.mps[self.tensor_count - 1].shape[1]:
+                raise ('tensors shape do not match')
+        self.mps[self.tensor_count] = tensor
+        self.tensor_count += 1
+
+    def OneSpinMeasurement(self, operator, spin_number):
+        spin_number *= 2
+        psi = cp.deepcopy(self.mps)
+        psi[spin_number] = np.einsum('ijk,jn->ink', psi[spin_number], operator)
+        expectation = self.braket(self.Dagger(), psi)
+        return expectation
+
+    def braket(self, phi_dagger, psi):
+        n = self.tensor_count
+        expectation = None
+        if self.bc == 'PBC':
+            element = np.einsum('ijk,njm->ikmn', psi[0], phi_dagger[0])
+            for i in range(1, n, 2):
+                element = np.einsum('ijkl,jn->inkl', element, psi[i])
+                element = np.einsum('ijkl,kn->ijnl', element, phi_dagger[i])
+                if i == n - 1:
+                    break
+                two_spins_contraction = np.einsum('ijk,njm->ikmn', psi[i + 1], phi_dagger[i + 1])
+                element = np.einsum('ijkl,jnmk->inml', element, two_spins_contraction)
+            element = np.einsum('ijkl->kl', element)
+            element = np.einsum('ij->',element)
+            expectation = element
+
+        if self.bc == 'OPEN':
+            element = np.einsum('ij,jk->ik', psi[0], phi_dagger[0])
+            for i in range(1, n, 2):
+                element = np.einsum('ij,in->nj', element, psi[i])
+                element = np.einsum('ij,jn->in', element, phi_dagger[i])
+                if i == n - 1:
+                    two_spins_contraction = np.einsum('ij,kj->ik', psi[i + 1], phi_dagger[i + 1])
+                    element = np.einsum('ij,ij->',element, two_spins_contraction)
+                    expectation = element
+                    break
+                two_spins_contraction = np.einsum('ijk,njm->ikmn', psi[i + 1], phi_dagger[i + 1])
+                element = np.einsum('ij,inmj->nm', element, two_spins_contraction)
+            element = np.einsum('ijkl->kl', element)
+            element = np.einsum('ij->',element)
+            expectation = element
+        return expectation
+
+    def Dagger(self):
+        psi_dagger = {}
+        for i in range(self.tensor_count):
+            psi_dagger[i] = np.conj(self.mps[i])
+        return psi_dagger
+
+    def TwoSpinsMeasurement(self, spin1, spin2, operator1, operator2):
+        spin1_idx = spin1 * 2
+        spin2_idx = spin2 * 2
+        if spin1_idx > self.tensor_count or spin2_idx > self.tensor_count:
+            raise ('There is no such spin')
+        psi = cp.deepcopy(self.mps)
+        psi[spin1_idx] = np.einsum('ijk,jn->ink', psi[spin1_idx], operator1)
+        psi[spin2_idx] = np.einsum('ijk,jn->ink', psi[spin2_idx], operator2)
+        return self.braket(self.Dagger(), psi)
+
+
+
