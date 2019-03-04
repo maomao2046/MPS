fix messages normalization
fixed by taking the sqrt of the dot product of messages with themselfs

FactorGraph.py
@@ -66,7 +66,7 @@ class Graph:
                     for item in neighbor_factors:
                         temp_message *= factor2node[item][n]
                     node2factor[n][f] = cp.copy(temp_message)
-                    node2factor[n][f] /= node2factor[n][f].dot(np.transpose(np.conj(node2factor[n][f])))
+                    node2factor[n][f] /= np.sqrt(node2factor[n][f].dot(np.transpose(np.conj(node2factor[n][f]))))
 
             for f in factors.keys():
                 for n in factors[f][0].keys():
@@ -78,7 +78,7 @@ class Graph:
                     for item in neighbor_nodes:
                         tensor *= self.broadcasting(node2factor[item][f], factors[f][0][item], tensor)
                     factor2node[f][n] = np.einsum(tensor, tensor_idx, message_idx)
-                    factor2node[f][n] /= factor2node[f][n].dot(np.transpose(np.conj(factor2node[f][n])))
+                    factor2node[f][n] /= np.sqrt(factor2node[f][n].dot(np.transpose(np.conj(factor2node[f][n]))))
                     if n == 'n1' and f == 'f4':
                         print(factor2node[f][n])
 

TN2FG.py
@@ -6,7 +6,7 @@ import wavefunction as wf
 import FactorGraph as fg
 
 
-psi = np.array([[[[1., 1.], [0., 0.]], [[1., 2.], [7.j, -2.]]], [[[1., 1.], [0., 0.]], [[1., 3.], [-4., 0.]]]])
+psi = np.array([[[[-1., 1.], [0., 0.]], [[0., 0.], [3.j, 0.]]], [[[1.j, 0.], [0., 4.]], [[-2.j, 0.], [5., 1.]]]])
 wf = wf.wavefunction()
 wf.addwf(psi, 4, 2)
 
@@ -73,13 +73,14 @@ n0_belief0 = []
 n0_belief1 = []
 n1_belief0 = []
 n1_belief1 = []
-for i in range(1, 29):
+for i in range(1, 100):
     graph.sum_product(i, 1e-6)
     graph.beliefs()
     n0_belief0.append(graph.node_belief['n0'][0])
     n0_belief1.append(graph.node_belief['n0'][1])
     n1_belief0.append(graph.node_belief['n1'][0])
     n1_belief1.append(graph.node_belief['n1'][1])
+    #print(graph.messages_f2n['f2']['n5'])
     print(i)
 
 for i in range(4):